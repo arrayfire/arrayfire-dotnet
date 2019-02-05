bdc77648638063aa73e66475744f13993c5a52b6
Oceania2018 <haiping008@gmail.com>
2019-02-05 07:20:55 -0600

    add UnitTest project

---
+++ test/ArrayFire.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArrayFire.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
