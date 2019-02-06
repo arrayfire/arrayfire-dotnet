0501781aca516a94f95c2bc1008b3437b0dcdb1f
Oceania2018 <haiping008@gmail.com>
2019-02-06 05:50:42 -0600

    1. Add FSharp example project back and upgrade to .net core 2.2. 2. reorganize file structure. 3. add UnitTest project

---
+++ test/ArrayFire.UnitTest/ArrayTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArrayFire.UnitTest
+{
+    /// <summary>
+    /// port from test\array.cpp
+    /// </summary>
+    [TestClass]
+    public class ArrayTest
+    {
+        [TestMethod]
+        public void ConstructorDefault()
+        {
+            Array a;
+        }
+    }
+}
