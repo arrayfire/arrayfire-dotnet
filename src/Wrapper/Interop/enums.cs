33cb86c9357756b0673823b80b73cac00c3cf8cc
haiping008 <haiping008@gmail.com>
2019-02-19 11:36:11 -0600

    Upgrade Interop to arrayfire v3.6

--- src/Wrapper/Interop/enums.cs
+++ src/Wrapper/Interop/enums.cs
@@ -170,11 +170,26 @@ namespace ArrayFire.Interop
 
 	public enum af_interp_type
 	{
-		AF_INTERP_NEAREST,  ///< Nearest Interpolation
-		AF_INTERP_LINEAR,   ///< Linear Interpolation
-		AF_INTERP_BILINEAR, ///< Bilinear Interpolation
-		AF_INTERP_CUBIC,    ///< Cubic Interpolation
-		AF_INTERP_LOWER     ///< Floor Indexed
+		AF_INTERP_NEAREST,         ///< Nearest Interpolation
+		AF_INTERP_LINEAR,          ///< Linear Interpolation
+		AF_INTERP_BILINEAR,        ///< Bilinear Interpolation
+		AF_INTERP_CUBIC,           ///< Cubic Interpolation
+		AF_INTERP_LOWER,           ///< Floor Indexed
+		// #if AF_API_VERSION >= 34
+		AF_INTERP_LINEAR_COSINE,   ///< Linear Interpolation with cosine smoothing
+		// #endif
+		// #if AF_API_VERSION >= 34
+		AF_INTERP_BILINEAR_COSINE, ///< Bilinear Interpolation with cosine smoothing
+		// #endif
+		// #if AF_API_VERSION >= 34
+		AF_INTERP_BICUBIC,         ///< Bicubic Interpolation
+		// #endif
+		// #if AF_API_VERSION >= 34
+		AF_INTERP_CUBIC_SPLINE,    ///< Cubic Interpolation with Catmull-Rom splines
+		// #endif
+		// #if AF_API_VERSION >= 34
+		AF_INTERP_BICUBIC_SPLINE,  ///< Bicubic Interpolation with Catmull-Rom splines
+		// #endif
 	}
 
 	public enum af_border_type
@@ -187,7 +202,12 @@ namespace ArrayFire.Interop
 		///
 		/// Out of bound values are symmetric over the edge
 		///
-		AF_PAD_SYM
+		AF_PAD_SYM,
+
+		///
+		/// Out of bound values are clamped to the edge
+		///
+		AF_PAD_CLAMP_TO_EDGE,
 	}
 
 	public enum af_connectivity
@@ -283,17 +303,6 @@ namespace ArrayFire.Interop
 		AF_NORM_EUCLID = AF_NORM_VECTOR_2, ///< The default. Same as AF_NORM_VECTOR_2
 	}
 
-	public enum af_colormap
-	{
-		AF_COLORMAP_DEFAULT = 0,    ///< Default grayscale map
-		AF_COLORMAP_SPECTRUM= 1,    ///< Spectrum map
-		AF_COLORMAP_COLORS  = 2,    ///< Colors
-		AF_COLORMAP_RED     = 3,    ///< Red hue map
-		AF_COLORMAP_MOOD    = 4,    ///< Mood map
-		AF_COLORMAP_HEAT    = 5,    ///< Heat map
-		AF_COLORMAP_BLUE    = 6     ///< Blue hue map
-	}
-
 	public enum af_image_format
 	{
 		AF_FIF_BMP          = 0,    ///< FreeImage Enum for Bitmap File
@@ -309,6 +318,15 @@ namespace ArrayFire.Interop
 		AF_FIF_EXR          = 29,   ///< FreeImage Enum for ILM OpenEXR File
 		AF_FIF_JP2          = 31,   ///< FreeImage Enum for JPEG-2000 File
 		AF_FIF_RAW          = 34    ///< FreeImage Enum for RAW Camera Image File
+	}
+
+	public enum af_moment_type
+	{
+		AF_MOMENT_M00 = 1,
+		AF_MOMENT_M01 = 2,
+		AF_MOMENT_M10 = 4,
+		AF_MOMENT_M11 = 8,
+		AF_MOMENT_FIRST_ORDER = AF_MOMENT_M00 | AF_MOMENT_M01 | AF_MOMENT_M10 | AF_MOMENT_M11
 	}
 
 	public enum af_homography_type
@@ -330,6 +348,40 @@ namespace ArrayFire.Interop
 		AF_ID = 0
 	}
 
+	public enum af_binary_op
+	{
+		AF_BINARY_ADD  = 0,
+		AF_BINARY_MUL  = 1,
+		AF_BINARY_MIN  = 2,
+		AF_BINARY_MAX  = 3
+	}
+
+	public enum af_random_engine_type
+	{
+		AF_RANDOM_ENGINE_PHILOX_4X32_10     = 100,                                  //Philox variant with N = 4, W = 32 and Rounds = 10
+		AF_RANDOM_ENGINE_THREEFRY_2X32_16   = 200,                                  //Threefry variant with N = 2, W = 32 and Rounds = 16
+		AF_RANDOM_ENGINE_MERSENNE_GP11213   = 300,                                  //Mersenne variant with MEXP = 11213
+		AF_RANDOM_ENGINE_PHILOX             = AF_RANDOM_ENGINE_PHILOX_4X32_10,      //Resolves to Philox 4x32_10
+		AF_RANDOM_ENGINE_THREEFRY           = AF_RANDOM_ENGINE_THREEFRY_2X32_16,    //Resolves to Threefry 2X32_16
+		AF_RANDOM_ENGINE_MERSENNE           = AF_RANDOM_ENGINE_MERSENNE_GP11213,    //Resolves to Mersenne GP 11213
+		AF_RANDOM_ENGINE_DEFAULT            = AF_RANDOM_ENGINE_PHILOX               //Resolves to Philox
+	}
+
+	public enum af_colormap
+	{
+		AF_COLORMAP_DEFAULT = 0,    ///< Default grayscale map
+		AF_COLORMAP_SPECTRUM= 1,    ///< Spectrum map (390nm-830nm, in sRGB colorspace)
+		AF_COLORMAP_COLORS  = 2,    ///< Colors, aka. Rainbow
+		AF_COLORMAP_RED     = 3,    ///< Red hue map
+		AF_COLORMAP_MOOD    = 4,    ///< Mood map
+		AF_COLORMAP_HEAT    = 5,    ///< Heat map
+		AF_COLORMAP_BLUE    = 6,    ///< Blue hue map
+		AF_COLORMAP_INFERNO = 7,    ///< Perceptually uniform shades of black-red-yellow
+		AF_COLORMAP_MAGMA   = 8,    ///< Perceptually uniform shades of black-red-white
+		AF_COLORMAP_PLASMA  = 9,    ///< Perceptually uniform shades of blue-red-yellow
+		AF_COLORMAP_VIRIDIS = 10    ///< Perceptually uniform shades of blue-green-yellow
+	}
+
 	public enum af_marker_type
 	{
 		AF_MARKER_NONE         = 0,
@@ -340,5 +392,40 @@ namespace ArrayFire.Interop
 		AF_MARKER_CROSS        = 5,
 		AF_MARKER_PLUS         = 6,
 		AF_MARKER_STAR         = 7
+	}
+
+	public enum af_canny_threshold
+	{
+		AF_CANNY_THRESHOLD_MANUAL    = 0, ///< User has to define canny thresholds manually
+		AF_CANNY_THRESHOLD_AUTO_OTSU = 1, ///< Determine canny algorithm thresholds using Otsu algorithm
+	}
+
+	public enum af_storage
+	{
+		AF_STORAGE_DENSE     = 0,   ///< Storage type is dense
+		AF_STORAGE_CSR       = 1,   ///< Storage type is CSR
+		AF_STORAGE_CSC       = 2,   ///< Storage type is CSC
+		AF_STORAGE_COO       = 3,   ///< Storage type is COO
+	}
+
+	public enum af_flux_function
+	{
+		AF_FLUX_QUADRATIC   = 1,    ///< Quadratic flux function
+		AF_FLUX_EXPONENTIAL = 2,    ///< Exponential flux function
+		AF_FLUX_DEFAULT     = 0     ///< Default flux function is exponential
+	}
+
+	public enum af_diffusion_eq
+	{
+		AF_DIFFUSION_GRAD = 1,      ///< Gradient diffusion equation
+		AF_DIFFUSION_MCDE = 2,      ///< Modified curvature diffusion equation
+		AF_DIFFUSION_DEFAULT = 0    ///< Default option is same as AF_DIFFUSION_GRAD
+	}
+
+	public enum af_topk_function
+	{
+		AF_TOPK_MIN     = 1,  ///< Top k min values
+		AF_TOPK_MAX     = 2,  ///< Top k max values
+		AF_TOPK_DEFAULT = 0   ///< Default option (max)
 	}
 }
